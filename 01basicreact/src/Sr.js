2b650e10426634e0373ecdfada2cc4db38a11a66
shashank raj <rshashank608@gmail.com>
2024-09-24 13:13:36 +0530

    third commit -understanding of react file naming conventions and understanding of export and import of file in vite and react

---
+++ 01basicreact/src/Sr.js
@@ -0,0 +1,8 @@
+function Sr(){
+    return( 
+
+        <h1>hello from shashank </h1>
+    )
+}
+
+export default Sr
---
+++ 01vitereact/src/Sr.jsx
@@ -0,0 +1,7 @@
+function Sr(){
+    return(
+        <h1>hello shashank in vite componenet</h1>
+    )
+}
+
+export default Sr;
