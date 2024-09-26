d3bbe7ee7dae70220c31366d102b2c6228739552
Shashank Raj <rshashank608@gmail.com>
2024-09-26 18:45:12 +0530

    react inner function working

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,13 @@
 import Sr from './Sr'
 function App() {
 
+const username='sharkmite'
+
   return (
   // <Sr></Sr>
   <>
   <Sr></Sr>
-
+    <h1>your username is {username}</h1>
   <p>hello shashank we use fragments to return multiple element</p>
    <h1>i hope you understand this things</h1>
   </>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+//import {jsx as jsx} from 'react/jsx-dev-runtime'
 
 
+function MyApp() {
+    return (
+        <div>
+            <h1>this is my app !</h1>
+        </div>
+    );
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// const reactElement={
+//   type:'a',
+//   props:{
+//       href:'https://google.com',
+//       target:'_blank'
+//   },
+//   children:'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https.//google.com" target="_blank">
+        visit to google
+    </a>
+);
+const username= 'sharkmite'
+const reactElement = React.createElement(
+    "a",
+    { href: "https://google.com", target: "_blank" },
+    "click to visit google",
+    username
+);
+
+createRoot(document.getElementById("root")).render(
+    //  reactElement
+    // // anotherElement
+    // //reactElement
     <App />
-  </StrictMode>,
-)
+);
